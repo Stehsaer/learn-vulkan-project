a79f801a80caa1aef87f6229c8339c97b7e09f09
Liu Hsin-chieh <Stehsaer@outlook.com>
2024-08-14 00:40:19 +0800

    Slightly changed api

--- vklib-hpp/core/include/vklib-cmdbuf.hpp
+++ vklib-hpp/core/include/vklib-cmdbuf.hpp
@@ -6,6 +6,32 @@
 
 namespace VKLIB_HPP_NAMESPACE
 {
+	struct Image_layout_state
+	{
+		vk::ImageLayout layout;
+		vk::AccessFlags access_flags;
+		uint32_t        queue_family_idx = vk::QueueFamilyIgnored;
+	};
+
+	inline vk::ImageMemoryBarrier image_layout_transit(
+		const Image&              image,
+		Image_layout_state        old_state,
+		Image_layout_state        new_state,
+		vk::ImageSubresourceRange subresource_range
+	)
+	{
+		return {
+			old_state.access_flags,
+			new_state.access_flags,
+			old_state.layout,
+			new_state.layout,
+			old_state.queue_family_idx,
+			new_state.queue_family_idx,
+			image,
+			subresource_range
+		};
+	}
+
 	class Command_buffer : public Child_resource<vk::CommandBuffer, Command_pool>
 	{
 		using Child_resource<vk::CommandBuffer, Command_pool>::Child_resource;
@@ -29,10 +55,11 @@ namespace VKLIB_HPP_NAMESPACE
 		/* State Transition */
 
 		vk::Result begin(bool one_time_submit = false) const;
-		void       end() const { data->child.end(); }
-		void       reset() const { data->child.reset(); }
 
-		void begin_render_pass(
+		inline void end() const { data->child.end(); }
+		inline void reset() const { data->child.reset(); }
+
+		inline void begin_render_pass(
 			const Render_pass&          render_pass,
 			const Framebuffer&          framebuffer,
 			const vk::Rect2D&           render_area,
@@ -40,26 +67,26 @@ namespace VKLIB_HPP_NAMESPACE
 			vk::SubpassContents         contents = vk::SubpassContents::eInline
 		) const;
 
-		void next_subpass(vk::SubpassContents contents = vk::SubpassContents::eInline) const
+		inline void next_subpass(vk::SubpassContents contents = vk::SubpassContents::eInline) const
 		{
 			data->child.nextSubpass(contents);
 		}
 
-		void end_render_pass() const { data->child.endRenderPass(); }
+		inline void end_render_pass() const { data->child.endRenderPass(); }
 
 		/* Draw */
 
-		void draw(uint32_t first_vertex, uint32_t vertex_count, uint32_t first_instance, uint32_t instance_count) const
+		inline void draw(uint32_t first_vertex, uint32_t vertex_count, uint32_t first_instance, uint32_t instance_count) const
 		{
 			data->child.draw(vertex_count, instance_count, first_vertex, first_instance);
 		}
 
-		void set_viewport(const vk::Viewport& viewport) const { data->child.setViewport(0, viewport); }
-		void set_scissor(const vk::Rect2D& scissor) const { data->child.setScissor(0, scissor); }
+		inline void set_viewport(const vk::Viewport& viewport) const { data->child.setViewport(0, viewport); }
+		inline void set_scissor(const vk::Rect2D& scissor) const { data->child.setScissor(0, scissor); }
 
 		/* Binding */
 
-		void bind_descriptor_sets(
+		inline void bind_descriptor_sets(
 			vk::PipelineBindPoint          bind_point,
 			const Pipeline_layout&         pipeline_layout,
 			uint32_t                       bind_offset,
@@ -69,21 +96,22 @@ namespace VKLIB_HPP_NAMESPACE
 			data->child.bindDescriptorSets(bind_point, pipeline_layout, bind_offset, descriptor_sets, {});
 		}
 
-		void bind_pipeline(
-			vk::PipelineBindPoint                       bind_point,
-			const Child_resource<vk::Pipeline, Device>& graphics_pipeline
-		) const
+		inline void bind_pipeline(vk::PipelineBindPoint bind_point, const Child_resource<vk::Pipeline, Device>& graphics_pipeline)
+			const
 		{
 			data->child.bindPipeline(bind_point, graphics_pipeline);
 		}
 
-		void bind_vertex_buffers(uint32_t first_binding, Array_proxy<vk::Buffer> vertex_buffer, Array_proxy<vk::DeviceSize> offsets)
-			const
+		inline void bind_vertex_buffers(
+			uint32_t                    first_binding,
+			Array_proxy<vk::Buffer>     vertex_buffer,
+			Array_proxy<vk::DeviceSize> offsets
+		) const
 		{
 			data->child.bindVertexBuffers(first_binding, vertex_buffer, offsets);
 		}
 
-		void push_constants(
+		inline void push_constants(
 			const Pipeline_layout& pipeline_layout,
 			vk::ShaderStageFlags   shader_stage,
 			uint32_t               offset,
@@ -95,7 +123,7 @@ namespace VKLIB_HPP_NAMESPACE
 		}
 
 		template <typename T>
-		void push_constants(
+		inline void push_constants(
 			const Pipeline_layout& pipeline_layout,
 			vk::ShaderStageFlags   shader_stage,
 			const T&               obj,
@@ -107,18 +135,13 @@ namespace VKLIB_HPP_NAMESPACE
 
 		/* Memory Manipulation*/
 
-		void copy_buffer(
-			const Buffer& dst_buffer,
-			const Buffer& src_buffer,
-			size_t        dst_offset,
-			size_t        src_offset,
-			size_t        size
-		) const
+		inline void copy_buffer(const Buffer& dst_buffer, const Buffer& src_buffer, size_t dst_offset, size_t src_offset, size_t size)
+			const
 		{
 			data->child.copyBuffer(src_buffer, dst_buffer, vk::BufferCopy(src_offset, dst_offset, size));
 		}
 
-		void copy_buffer_to_image(
+		inline void copy_buffer_to_image(
 			const Image&                     dst_image,
 			const Buffer&                    src_buffer,
 			vk::ImageLayout                  image_layout,
@@ -128,7 +151,7 @@ namespace VKLIB_HPP_NAMESPACE
 			data->child.copyBufferToImage(src_buffer, dst_image, image_layout, copy_region);
 		}
 
-		void blit_image(
+		inline void blit_image(
 			vk::Image                  src_image,
 			vk::ImageLayout            src_image_layout,
 			vk::Image                  dst_image,
@@ -138,6 +161,18 @@ namespace VKLIB_HPP_NAMESPACE
 		) const
 		{
 			data->child.blitImage(src_image, src_image_layout, dst_image, dst_image_layout, regions, blit_filter);
+		}
+
+		inline void pipeline_barrier(
+			vk::PipelineStageFlags               src_stage,
+			vk::PipelineStageFlags               dst_stage,
+			Array_proxy<vk::MemoryBarrier>       memory_barriers,
+			Array_proxy<vk::BufferMemoryBarrier> buffer_barriers,
+			Array_proxy<vk::ImageMemoryBarrier>  image_barriers,
+			vk::DependencyFlags                  dependency_flags = {}
+		) const
+		{
+			data->child.pipelineBarrier(src_stage, dst_stage, dependency_flags, memory_barriers, buffer_barriers, image_barriers);
 		}
 
 		void layout_transit(
